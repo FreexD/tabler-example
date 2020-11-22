edde95b0ad32ceda956d4d25d0cfdccad70f6569
Michal Wolny <michal.wolny@versum.pl>
2020-11-22 18:44:28 +0100

    regions

--- ssr/app.js
+++ ssr/app.js
@@ -26,9 +26,16 @@ function lastSyncTime() {
 // server side chart data generation
 const getChartData = () => {
   // area chart
-  const profits = Array.from({ length: 30 }, () =>
-    Math.floor(Math.random() * 40)
+  const revenue = fs.readFileSync(
+    path.join(__dirname, "revenue.txt"),
+    "utf8",
+    function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+    }
   );
+  const profits = JSON.parse(revenue);
   // stacked bar chart
   const web = Array.from({ length: 37 }, (item, idx) =>
     Math.floor(Math.random() * idx * 4)
@@ -67,8 +74,6 @@ app.get("/:level?", function (req, res) {
       if (err) {
         return console.log(err);
       }
-      console.log(data);
-      content = parseInt(data);
     }
   );
 
@@ -79,6 +84,98 @@ app.get("/:level?", function (req, res) {
       lastSync: lastSyncTime(),
       chartData: getChartData(),
       level: level,
+      sales: parseInt(data),
+    },
+    {},
+    function (err, template) {
+      if (err) {
+        throw err;
+      } else {
+        res.end(template);
+      }
+    }
+  );
+});
+
+// Render index.ejs file
+app.get("/0/:level?", function (req, res) {
+  if ((req.params.level && isNaN(+req.params.level)) || req.params.level === '0') {
+    res.redirect("/");
+  }
+  const level = +req.params.level || 0;
+  if (level < 0) {
+    res.redirect("/");
+  }
+  if (level > 5) {
+    res.redirect('/5');
+  }
+
+  const data = fs.readFileSync(
+    path.join(__dirname, "data.txt"),
+    "utf8",
+    function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data);
+      content = parseInt(data);
+    }
+  );
+
+  // Render page using renderFile method
+  ejs.renderFile(
+    "ssr/region0.ejs",
+    {
+      lastSync: lastSyncTime(),
+      chartData: getChartData(),
+      level: level,
+      region: 0,
+      sales: parseInt(data),
+    },
+    {},
+    function (err, template) {
+      if (err) {
+        throw err;
+      } else {
+        res.end(template);
+      }
+    }
+  );
+});
+
+// Render index.ejs file
+app.get("/1/:level?", function (req, res) {
+  if ((req.params.level && isNaN(+req.params.level)) || req.params.level === '0') {
+    res.redirect("/");
+  }
+  const level = +req.params.level || 0;
+  if (level < 0) {
+    res.redirect("/");
+  }
+  if (level > 5) {
+    res.redirect('/5');
+  }
+
+  const data = fs.readFileSync(
+    path.join(__dirname, "data.txt"),
+    "utf8",
+    function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data);
+      content = parseInt(data);
+    }
+  );
+
+  // Render page using renderFile method
+  ejs.renderFile(
+    "ssr/region1.ejs",
+    {
+      lastSync: lastSyncTime(),
+      chartData: getChartData(),
+      level: level,
+      region: 1,
       sales: parseInt(data),
     },
     {},
